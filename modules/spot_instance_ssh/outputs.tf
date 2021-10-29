139f67e7d1f53064e22b9e3431fa56b0e299ee32
Scott Giminiani <scottg489@gmail.com>
2021-10-29 19:37:36 -0400

    Allow access to the public_dns name for a spot instance
    This is used by CloudFront to know where to forward requests to.

--- modules/spot_instance_ssh/outputs.tf
+++ modules/spot_instance_ssh/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = aws_spot_instance_request.spot_instance_request.public_ip
 }
+
+output "public_dns" {
+  value = aws_spot_instance_request.spot_instance_request.public_dns
+}
