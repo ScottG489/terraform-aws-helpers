066e1bc5abb1871a42b99f11dfbf8e5d0b744d66
Scott Giminiani <scottg489@gmail.com>
2022-03-03 01:07:08 -0500

    Make spot_instance_id available as output from spot_instance_ssh module

--- modules/spot_instance_ssh/outputs.tf
+++ modules/spot_instance_ssh/outputs.tf
@@ -1,3 +1,7 @@
+output "spot_instance_id" {
+  value = aws_spot_instance_request.spot_instance_request.spot_instance_id
+}
+
 output "public_ip" {
   value = aws_spot_instance_request.spot_instance_request.public_ip
 }
