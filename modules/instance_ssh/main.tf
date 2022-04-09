b617b669234470163e17899a1fc607054be52e24
Scott Giminiani <scottg489@gmail.com>
2022-04-09 16:12:10 -0400

    Allow specifying instance type and volume size for ssh instance

--- modules/instance_ssh/main.tf
+++ modules/instance_ssh/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "instance" {
   ami           = var.ami
-  instance_type = "t2.small"
+  instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
   key_name = aws_key_pair.key_pair.key_name
 
   root_block_device {
     volume_type           = "gp2"
-    volume_size           = 50
+    volume_size           = var.volume_size
   }
 
   tags = {
--- modules/instance_ssh/variables.tf
+++ modules/instance_ssh/variables.tf
@@ -3,4 +3,9 @@ variable "ami" {
   type = string
   default = "ami-09dd2e08d601bff67"
 }
+variable "instance_type" {}
+variable "volume_size" {
+  type = number
+  default = 50
+}
 variable "public_key" {}
