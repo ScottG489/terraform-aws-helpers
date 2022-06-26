921bb2ae541685f96dfd561046d45f2692a24c97
Scott Giminiani <scottg489@gmail.com>
2022-06-26 18:44:35 -0400

    Allow access to the website buckets' regional domain name
    This is needed by CloudFront as part of making a https S3 website.

--- modules/s3_website_subdomain/outputs.tf
+++ modules/s3_website_subdomain/outputs.tf
@@ -6,6 +6,10 @@ output "website_endpoint" {
   value = aws_s3_bucket.website_bucket.website_endpoint
 }
 
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+}
+
 output "website_hosted_zone_id" {
   value = aws_s3_bucket.website_bucket.hosted_zone_id
 }
