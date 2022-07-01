938f64926feb7154ed092ae5b357a9dbee6f5e43
Scott Giminiani <scottg489@gmail.com>
2022-06-30 22:54:09 -0400

    Use updated method of created S3 buckets with terraform
    This change to newer versions of terraform split up the creation of S3
    buckets into multiple different resources.

--- modules/s3_website_subdomain/main.tf
+++ modules/s3_website_subdomain/main.tf
@@ -1,12 +1,24 @@
 resource "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
-  acl    = "public-read"
-  policy = templatefile("${path.module}/policy-template.json", { bucket_name: var.bucket_name })
   force_destroy = true
+}
 
-  website {
-    index_document = "index.html"
+resource "aws_s3_bucket_website_configuration" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
+}
+
+resource "aws_s3_bucket_acl" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = templatefile("${path.module}/policy-template.json", { bucket_name: var.bucket_name })
 }
 
 resource "aws_route53_record" "website_record_A" {
