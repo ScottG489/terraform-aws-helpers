e4e99f65659dd8992c2f26e8b4a65fafc8e83833
Scott Giminiani <scottg489@gmail.com>
2023-04-19 20:43:17 -0400

    Fix permissions on S3 website subdomain buckets so they can be public
    AWS recently made changes to how S3 buckets are created. More
    information can be found here:
    
    https://github.com/hashicorp/terraform-provider-aws/issues/28353

--- modules/s3_website_subdomain/main.tf
+++ modules/s3_website_subdomain/main.tf
@@ -12,11 +12,20 @@ resource "aws_s3_bucket_website_configuration" "website_bucket" {
 }
 
 resource "aws_s3_bucket_acl" "website_bucket" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.website_bucket,
+    aws_s3_bucket_ownership_controls.website_bucket,
+  ]
+
   bucket = aws_s3_bucket.website_bucket.id
   acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "website_bucket" {
+  depends_on = [
+    aws_s3_bucket_acl.website_bucket,
+  ]
+
   bucket = aws_s3_bucket.website_bucket.id
   policy = templatefile("${path.module}/policy-template.json", { bucket_name: var.bucket_name })
 }
@@ -30,5 +39,21 @@ resource "aws_route53_record" "website_record_A" {
     zone_id                = var.s3_website_hosted_zone_id
     name                   = var.s3_website_record_alias_name
     evaluate_target_health = false
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
 }
